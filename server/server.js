2cc0d8fea2bb4c4ba3e77ef8189260c75a83b5d6
Jotas <jproodian721@gmail.com>
2023-04-06 23:16:02 -0500

    install dotenv, concurrently dependencies, set up run and install scripts, create react app/client folder, connect server to testing postgres db server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+// import connection info to db
+const client = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// connect to pg db
+client.connect((err) => {
+    if (err) {
+        console.log('connection error', err.stack)
+    } else {
+        console.log('connected');
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Libaby is now listening on port ${PORT}`)
